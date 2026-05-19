5aab4b46d7d73acb6e37ce8483f634433217ad63
Yeisson <yei_rob24@hotmail.com>
2026-05-19 15:40:02 -0600

    add: changes to app styles

---
+++ lib/config/themes/app_styles.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+Color primary = Color(0xFF687DAF);
+
+class AppStyles {
+  static Color primaryColor = primary;
+  static Color textColor = Color(0xFF3B3B3B);
+
+  static TextStyle headLineStyle1 = TextStyle(
+    fontSize: 26,
+    fontWeight: .w500,
+    color: textColor,
+  );
+
+  static TextStyle headLineStyle3 = TextStyle(
+    fontSize: 17,
+    fontWeight: .w500,
+  );
+}
--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,31 +1,81 @@
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:ticket_booking_app/config/themes/app_styles.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    final size = MediaQuery.sizeOf(context);
 
+    return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.blueGrey,
 
         leading: IconButton(
           onPressed: () {},
-          icon: Icon(Icons.navigate_before, color: Colors.white),
+          icon: Icon(FluentIcons.star_24_filled, color: Colors.white),
         ),
 
         actions: [
           IconButton(
             onPressed: () {},
-            icon: Icon(Icons.person, color: Colors.white),
+            icon: Icon(FluentIcons.folder_24_filled, color: Colors.white),
           ),
         ],
-        
+
         title: Text('Book Tickets', style: TextStyle(color: Colors.white)),
         centerTitle: true,
       ),
-      body: Center(child: Text('Hello World')),
+      body: ListView(
+        children: [
+          Container(
+            // color: Colors.blue,
+            padding: .symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: .spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: .start,
+                      children: [
+                        Text('Good morning', style: AppStyles.headLineStyle3),
+
+                        SizedBox(height: 6),
+
+                        Text('Book Tickets', style: AppStyles.headLineStyle1),
+                      ],
+                    ),
+
+                    // Text('Imageee'),
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: .circular(10),
+                      ),
+                    ),
+                  ],
+                ),
+
+                Row(
+                  mainAxisAlignment: .spaceBetween,
+                  children: [Text('ICON'), Text('HINTEXT')],
+                ),
+
+                Text('Buscador'),
+              ],
+            ),
+          ),
+          Container(),
+          Container(),
+          Container(),
+          Container(),
+        ],
+      ),
     );
   }
 }
