8e6094cab4a8245be4b1a742e1d682f4555f0ad8
Yeisson <yei_rob24@hotmail.com>
2026-05-19 17:20:14 -0600

    add: changes to app styles

--- lib/config/themes/app_styles.dart
+++ lib/config/themes/app_styles.dart
@@ -4,16 +4,25 @@ Color primary = Color(0xFF687DAF);
 
 class AppStyles {
   static Color primaryColor = primary;
+  static Color bgColor = Color(0xFFEEEDF2);
   static Color textColor = Color(0xFF3B3B3B);
 
   static TextStyle headLineStyle1 = TextStyle(
     fontSize: 26,
-    fontWeight: .w500,
+    fontWeight: .bold,
+    color: textColor,
+  );
+  static TextStyle headLineStyle2 = TextStyle(
+    fontSize: 21,
+    fontWeight: .bold,
     color: textColor,
   );
 
-  static TextStyle headLineStyle3 = TextStyle(
-    fontSize: 17,
+  static TextStyle headLineStyle3 = TextStyle(fontSize: 17, fontWeight: .w500);
+
+  static TextStyle textStyle = TextStyle(
+    fontSize: 16,
     fontWeight: .w500,
+    color: textColor,
   );
 }
